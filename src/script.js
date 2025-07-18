22c92b529922423d2c2f5e74dcc959eb7754f84d
Andiswa-Dlamini <adlamini179@gmail.com>
2025-07-18 12:22:49 +0200

    Improved UX

--- src/script.js
+++ src/script.js
@@ -6,20 +6,24 @@ const poem = (response) => {
     autoStart: true,
 });
 } 
-const generatePoem = () => {
+const generatePoem = (event) => {
+    event.preventDefault();
+
     const userInput = document.getElementById("user-input").value;
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
 
     let apiKey = "1c716bbf1f913c803d3d7b147d0baaot";
-    let context = "Make sure to provide a clear, short and precise answer";
+    let context = "You are a poem expert who loves generating poems. Your mission is to generate a 3 line poem basic HTML and separate each line with a <br />.Don't include a title. Don't display html writimg. Make sure to follow the user instructions.";
     let prompt = `Please generate a tech related poem ${userInput}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     document.getElementById("generated-poem").textContent = `Generating a poem about ${userInput} ...`
-
+    document.getElementById("generated-poem").classList.toggle("hidden");
 
     axios.get(apiUrl).then(poem);
 };
 
-const buttonElement = document.querySelector("#generate-btn");
+const buttonElement = document.querySelector(".generate-btn");
 buttonElement.addEventListener("click", generatePoem);
 
+// everytime a button is clicked it must display a block
